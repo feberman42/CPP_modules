8566a38035655d68d3800a0c67f61958a2bd4025
feberman42 <feberman@student.42berlin.de>
2024-02-23 15:57:48 +0100

    more testing

--- cpp0/ex01/main.cpp
+++ cpp0/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 17:02:11 by feberman          #+#    #+#             */
-/*   Updated: 2024/02/23 15:40:17 by feberman         ###   ########.fr       */
+/*   Updated: 2024/02/23 15:56:45 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,28 @@
 
 int	main(void)
 {
-	PhoneBook pb;
+	// PhoneBook 	pb;
+	std::string	buf;
 
-	std::cout << pb;
+	while (true)
+	{
+		std::cout << "> ";
+		std::cin >> buf;
+		if (buf.compare("EXIT") == 0)
+			return (0);
+		else if (buf.compare("ADD") == 0)
+		{
+			//add new contact
+		}
+		else if (buf.compare("SEARCH") == 0)
+		{
+			//search
+		}
+		else
+		{
+			std::cout << "Command not found." << std::endl;
+			std::cout << "Use ADD, SEARCH or EXIT" << std::endl;
+		}
+	}
 	return (0);
 }
