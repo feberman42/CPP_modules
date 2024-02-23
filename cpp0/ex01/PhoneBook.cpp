ac654f5c1ad4f9853f9d5ad4b4916f0d9b7fc277
feberman42 <feberman@student.42berlin.de>
2024-02-23 15:41:14 +0100

    implemented PhoneBook Class

--- cpp0/ex01/PhoneBook.cpp
+++ cpp0/ex01/PhoneBook.cpp
@@ -6,8 +6,39 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 17:01:40 by feberman          #+#    #+#             */
-/*   Updated: 2024/02/22 17:03:34 by feberman         ###   ########.fr       */
+/*   Updated: 2024/02/23 15:39:23 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
+
+PhoneBook::PhoneBook(void)
+{
+	std::cout << "PhoneBook constructor called." << std::endl;
+	this->active = 0;
+	return;
+}
+
+PhoneBook::~PhoneBook(void)
+{
+	std::cout << "PhoneBook destructor called." << std::endl;
+	return;
+}
+
+Contact	*PhoneBook::get_active_contact(void)
+{
+	Contact	*c;
+
+	c = &this->contacts[this->active];
+	this->active++;
+	if (this->active == 8)
+		this->active = 0;
+	return (c);
+}
+
+std::ostream	&operator<<(std::ostream &os, PhoneBook const &c)
+{
+	(void)c;
+	os << "PhoneBook print called." << std::endl;
+	return (os);
+}
--- cpp0/ex01/PhoneBook.hpp
+++ cpp0/ex01/PhoneBook.hpp
@@ -6,13 +6,31 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 17:01:29 by feberman          #+#    #+#             */
-/*   Updated: 2024/02/22 17:03:19 by feberman         ###   ########.fr       */
+/*   Updated: 2024/02/23 15:32:18 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
+# include <string>
+# include <iostream>
+# include "Contact.hpp"
 
+class PhoneBook
+{
+	public:
+		PhoneBook(void);
+		~PhoneBook(void);
+		
+		Contact	*get_active_contact(void);
+		
+	private:
+		Contact	contacts[8];
+		int		active;
+
+};
+
+std::ostream	&operator<<(std::ostream &os, PhoneBook const &c);
 
 #endif
