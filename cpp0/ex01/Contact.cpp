f022177242eff5211370d539a06e623bcc91c8e1
feberman42 <feberman@student.42berlin.de>
2024-02-23 15:41:26 +0100

    implemented Contact Class

--- cpp0/ex01/Contact.cpp
+++ cpp0/ex01/Contact.cpp
@@ -6,9 +6,26 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 17:02:04 by feberman          #+#    #+#             */
-/*   Updated: 2024/02/22 17:04:42 by feberman         ###   ########.fr       */
+/*   Updated: 2024/02/23 15:25:47 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
+Contact::Contact(void)
+{
+	std::cout << "Contact constructor called." << std::endl;
+	return ;
+}
+Contact::~Contact(void)
+{
+	std::cout << "Contact destructor called." << std::endl;
+	return ;
+}
+		
+std::ostream	&operator<<(std::ostream &os, Contact const &c)
+{
+	(void)c;
+	os << "Contact print called." << std::endl;
+	return (os);
+}
--- cpp0/ex01/Contact.hpp
+++ cpp0/ex01/Contact.hpp
@@ -6,21 +6,32 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 17:01:56 by feberman          #+#    #+#             */
-/*   Updated: 2024/02/22 17:05:39 by feberman         ###   ########.fr       */
+/*   Updated: 2024/02/23 15:22:19 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 # define CONTACT_HPP
 
+# include <string>
+# include <iostream>
+
 class Contact
 {
 	public:
-		Contact();
-		~Contact();
+		Contact(void);
+		~Contact(void);
 		
-	private:
 		
+		std::string	first_name;
+		std::string	last_name;
+		std::string	nick_name;
+		std::string	phone_number;
+		std::string	secret;
+		
+
 };
+
+std::ostream	&operator<<(std::ostream &os, Contact const &c);
 
 #endif
