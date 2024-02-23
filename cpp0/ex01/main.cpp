877ccda37a3a9fe1d53ef06a3b702bc7c9a9f497
feberman42 <feberman@student.42berlin.de>
2024-02-23 15:41:44 +0100

    testing

--- cpp0/ex01/main.cpp
+++ cpp0/ex01/main.cpp
@@ -6,7 +6,17 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 17:02:11 by feberman          #+#    #+#             */
-/*   Updated: 2024/02/22 17:02:12 by feberman         ###   ########.fr       */
+/*   Updated: 2024/02/23 15:40:17 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Contact.hpp"
+#include "PhoneBook.hpp"
+
+int	main(void)
+{
+	PhoneBook pb;
+
+	std::cout << pb;
+	return (0);
+}
