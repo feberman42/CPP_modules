563ccb67bcecb378d401b470ff0d88c11c3b9e1d
feberman42 <feberman@student.42berlin.de>
2024-11-12 21:49:10 +0100

    started ex02

---
+++ cpp9/ex02/input_validation.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_validation.cpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 21:35:51 by feberman          #+#    #+#             */
+/*   Updated: 2024/11/12 21:48:29 by feberman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <vector>
+
+bool	validate_input(int argc, char *argv[])
+{
+	std::vector<int>	vec;
+	std::size_t			i;
+
+	if (argc < 2)
+		return (false);
+	i = 1;
+	while (i < argc)
+	{
+		
+	}
+}
